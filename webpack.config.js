56482e01240a268effa51483c363c59e5950151a
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2024-03-23 09:39:43 +0000

    remove prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,59 @@
-const path = require('path');
+const path = require("path");
 
-const PrettierPlugin = require("./_utils/prettier.js");
-const cleanStack = require("./_utils/clean-stack.js");
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
-const WebpackErrorReporting = require('bc-webpack-error-reporting-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+const WebpackErrorReporting = require("bc-webpack-error-reporting-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const port = 3000;
 let publicUrl = `http://localhost:${port}`;
-if(process.env.GITPOD_WORKSPACE_URL){
-  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split("://");
   publicUrl = `${port}-${host}`;
 }
 
 module.exports = {
-  mode: 'development',
-  entry: ['./src/app.js'],
+  mode: "development",
+  entry: ["./src/app.js"],
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'main.bundle.js',
-    sourceMapFilename: '[name].js.map'
+    path: path.resolve(__dirname, "public"),
+    filename: "main.bundle.js",
+    sourceMapFilename: "[name].js.map",
   },
   devtool: "source-map",
   devServer: {
     historyApiFallback: true,
     public: publicUrl,
-    stats: 'errors-warnings',
+    stats: "errors-warnings",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['babel-loader', 'eslint-loader']
+        use: ["babel-loader", "eslint-loader"],
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif|ico)$/,
         use: {
-          loader: 'file-loader',
-          options: { name: '[name].[ext]' }
-        }
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
+        },
       },
       {
         test: /\.html$/i,
         use: {
-          loader: 'html-loader',
+          loader: "html-loader",
           options: {
-            attributes: false
-          }
-        }
-      }
-    ]
+            attributes: false,
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new WebpackErrorReporting({
@@ -64,18 +62,15 @@ module.exports = {
       token: process.env.BC_ASSETS_TOKEN,
       compiler: "webpack",
       language: "html,css,javascript",
-      framework: "vanillajs"
+      framework: "vanillajs",
     }),
     new FriendlyErrorsWebpackPlugin({
-        // additionalFormatters: [cleanStack]
+      // additionalFormatters: [cleanStack]
     }),
     new ErrorOverlayPlugin(),
     new HtmlWebpackPlugin({
-        filename: "index.html",
-        template: "src/index.html"
+      filename: "index.html",
+      template: "src/index.html",
     }),
-    new PrettierPlugin({
-        failSilently: true
-    }),
-  ]
+  ],
 };
